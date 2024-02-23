5331b807037c1af6299dce3e5a60f885a68e6128
Gerald Encabo <geraldencabo@gmail.com>
2024-02-22 21:36:43 -0500

    Navbar active update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,69 @@
 import '@/styles/navbar.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { navbarTypes } from '@/types/TypesList';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
 
-  const navbarList = Object.values(navbarTypes);
-  const [click, setClick] = useState<boolean>(false);
+   const navbarList = Object.values(navbarTypes);
+   const [click, setClick] = useState<boolean>(false);
+   const [count, setCount] = useState<number>(0);
 
-  const handleClick = () => setClick(!click);
+   const handleClick = () => setClick(!click);
 
-  const handleMenuClick = (navList: string) => {
-      const scrollToId = document.getElementById(navList.toLowerCase());
-      const location = scrollToId?.offsetTop!;
+   const handleMenuClick = (navList: string) => {
+         const scrollToId = document.getElementById(navList.toLowerCase());
+         const location = scrollToId?.offsetTop!;
 
-      window.scrollTo({
-         left: 0,
-         top: location - 80,
-         behavior: "smooth",
-      })
+         window.scrollTo({
+            left: 0,
+            top: location - 80,
+            behavior: "smooth",
+         })
 
-      // Handle close menu in mobile view
-      setClick(false);
-  }
+         // Handle close menu in mobile view
+         setClick(false);
+   }
 
-  return (
-    <header className='navbar'>
-        <div className='navbar-wrapper'>
-            <div className='navbar-container container'>
-                 <div className='navbar-icon'>
-                     <NavLink 
-                        to={navbarTypes.About.toLowerCase()} 
-                        onClick={() => handleMenuClick(navbarTypes.About.toLowerCase())} 
-                        className='navbar-logo'
-                     >
-                        GE.
-                     </NavLink>
-                </div>
-                <div className="navbar-mini-device" onClick={handleClick}>
-                  { click ? <FaTimes /> : <FaBars /> }
-                </div>
-                <ul className={ click ? 'navbar-menu active' : 'navbar-menu' }>
-                  {
-                     navbarList.map((navList: string, id: number) => (
-                        <li className='navbar-item' key={id}>
-                           <NavLink
-                              to={navList.toLowerCase()}
-                              onClick={() => handleMenuClick(navList)} 
-                              className='navbar-links'
-                           >
-                              {navList}
-                           </NavLink>
-                        </li>
-                     ))
-                  }
-                </ul>
-            </div>
-        </div>
-    </header>
-  )
+   return (
+      <header className='navbar'>
+         <div className='navbar-wrapper'>
+               <div className='navbar-container container'>
+                  <div className='navbar-icon'>
+                        <NavLink 
+                           to={navbarTypes.About.toLowerCase()} 
+                           onClick={() => handleMenuClick(navbarTypes.About.toLowerCase())} 
+                           className='navbar-logo'
+                        >
+                           GE.
+                        </NavLink>
+                  </div>
+                  <div className="navbar-mini-device" onClick={handleClick}>
+                     { click ? <FaTimes /> : <FaBars /> }
+                  </div>
+                  <ul className={ click ? 'navbar-menu active' : 'navbar-menu' }>
+                     {
+                        navbarList.map((navList: string, id: number) => (
+                           <li className='navbar-item' key={id}>
+                              <NavLink
+                                 to={navList.toLowerCase()}
+                                 onClick={() => {
+                                    setCount(id);
+                                    handleMenuClick(navList)
+                                 }} 
+                                 className={`navbar-links ${id === count ? 'active' : ''} `}
+                              >
+                                 {navList}
+                              </NavLink>
+                           </li>
+                        ))
+                     }
+                  </ul>
+               </div>
+         </div>
+      </header>
+   )
 }
 
 export default Navbar
