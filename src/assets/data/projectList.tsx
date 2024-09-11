f8a721ca2c5dad8b00588ab5fbdec4685dba8b80
Gerald Encabo <geraldencabo@gmail.com>
2024-09-10 23:39:04 -0400

    Update projectList.tsx

--- src/assets/data/projectList.tsx
+++ src/assets/data/projectList.tsx
@@ -14,7 +14,7 @@ export const projectList: Array<projectTypes> = [
         alt: 'Ramen House home page',
         title: 'Ramen House',
         desc: 'This is a full stack responsive e-commerce webpage focusing on the food industry of Ramen or Japanese Noodle soup.',
-        teckStack: 'HTML5, CSS3, JavaScript (ES6), TypeScript, Styled-Components, JSON, ReactJS, Redux, Vite, Axios, Node.js, Webpack, Vercel, Github'
+        teckStack: 'HTML5, CSS3, JavaScript (ES6), TypeScript, Styled-Components, JSON, ReactJS, Redux, Vite, Axios, Node.js, Webpack, Github, AWS technologies (Amplify and S3)'
     },
     {
         id: 2,
